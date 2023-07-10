e42f7980a8a37e6f86fbd2db572daeacc87fedb4
qin <2045251631@qq.com>
2023-07-10 18:47:59 +0800

    first commit

---
+++ src/main/java/com/example/petshopuser/PetShopUserApplication.java
@@ -0,0 +1,13 @@
+package com.example.petshopuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetShopUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetShopUserApplication.class, args);
+    }
+
+}
