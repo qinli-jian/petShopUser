7a4061acbe46e43fef9d63b77e5c777f1bc3c9f2
qin <2045251631@qq.com>
2023-07-17 18:29:51 +0800

    完成评论模块接口

---
+++ src/main/java/com/example/petshopuser/controller/CommentController.java
@@ -0,0 +1,116 @@
+package com.example.petshopuser.controller;
+
+import com.example.petshopuser.common.Constants;
+import com.example.petshopuser.entity.Comment;
+import com.example.petshopuser.entity.ReturnObj;
+import com.example.petshopuser.repository.CommentRepository;
+import com.example.petshopuser.service.impl.CommentServiceImpl;
+import com.example.petshopuser.utils.Utils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/comment")
+public class CommentController {
+
+    @Resource
+    private CommentServiceImpl commentService;
+
+    @GetMapping("/test")
+    public ReturnObj text(){
+        ReturnObj returnObj = new ReturnObj();
+        commentService.testGetData();
+        return returnObj;
+    }
+
+    @GetMapping("/listbyCommodityId")
+    public ReturnObj getCommentListByCommodityId(@RequestParam(value = "commodity_id") String commodity_id,@RequestParam(value = "pageNum") int pageNum,@RequestParam(value = "pageSize") int pageSize){
+        ReturnObj returnObj = new ReturnObj();
+
+        List<Comment> comments = commentService.getAllByCommodityId(commodity_id,pageNum,pageSize);
+        if(comments!=null){
+            returnObj.setCode(Constants.CODE_200);
+            returnObj.setMsg("success");
+            returnObj.setData(comments);
+        }else{
+            returnObj.setCode(Constants.CODE_500);
+            returnObj.setMsg("failed");
+        }
+        return returnObj;
+    }
+
+    // 添加评论
+    @PostMapping("/addcomment")
+    public ReturnObj addcomment(@RequestBody Map<String,Object> addCommentInfo) {
+        ReturnObj returnObj = new ReturnObj();
+        String user_id;
+        String content;
+        ArrayList<String> images;
+        Integer rating;
+        String reply_to_comment_id = "-1";
+        try {
+            user_id = (String) addCommentInfo.get("user_id");
+            content = (String) addCommentInfo.get("content");
+            rating = (Integer) addCommentInfo.get("rating");
+            images = (ArrayList<String>) addCommentInfo.get("images");
+
+            if (addCommentInfo.containsKey("reply_to_comment_id")) {
+                reply_to_comment_id = (String) addCommentInfo.get("reply_to_comment_id");
+                if(!reply_to_comment_id.equals("-1")){
+                    rating = -1;
+                }
+            }
+//            Integer rating_int = Integer.valueOf(rating);
+        } catch (Exception e) {
+            returnObj.setCode(Constants.CODE_400);
+            returnObj.setMsg("参数错误");
+            return returnObj;
+        }
+        if(!addCommentInfo.containsKey("commodity_id")){
+            returnObj.setCode(Constants.CODE_500);
+            returnObj.setMsg("params error");
+            return returnObj;
+        }
+        String commodity_id = (String) addCommentInfo.get("commodity_id");
+        String timestamp = Utils.nowTime();
+
+        Comment comment = new Comment(user_id, commodity_id, content, images, timestamp, rating, reply_to_comment_id);
+        // 写入数据库
+        int flag = commentService.addComment(comment);
+        if(flag==1){
+            returnObj.setCode(Constants.CODE_200);
+            returnObj.setMsg("success");
+        }else{
+            returnObj.setCode(Constants.CODE_500);
+            returnObj.setMsg("failed");
+        }
+        return returnObj;
+    }
+
+    // 获取平均评分
+    @GetMapping("/avgCommodityRating")
+    public ReturnObj avgCommodityRating(@RequestParam(value = "commodity_id")String commodity_id){
+        ReturnObj returnObj = new ReturnObj();
+        try{
+            double avgRating = commentService.getAvgCommodityRating(commodity_id);
+            returnObj.setCode(Constants.CODE_200);
+            returnObj.setMsg("success");
+            HashMap<String, Double> data = new HashMap<String, Double>();
+            data.put("avgRating",avgRating);
+            returnObj.setData(data);
+        }catch (Exception e){
+            returnObj.setCode(Constants.CODE_500);
+            returnObj.setMsg("failed");
+        }
+        return returnObj;
+    }
+
+
+
+}
---
+++ src/main/java/com/example/petshopuser/service/impl/CommentServiceImpl.java
@@ -0,0 +1,94 @@
+package com.example.petshopuser.service.impl;
+
+import com.example.petshopuser.entity.Comment;
+import com.example.petshopuser.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CommentServiceImpl {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+    private final CommentRepository commentRepository;
+
+    @Autowired
+    public CommentServiceImpl(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+
+    public void testGetData(){
+        List<Comment> all = mongoTemplate.findAll(Comment.class);
+        System.out.println("评论查询测试");
+        System.out.println(all);
+    }
+
+    // 根据商品ID 进行查询评论
+    public List<Comment> getAllByCommodityId(String commodity_id,int pageNum,int pageSize) {
+        System.out.println("分页");
+        System.out.println(pageNum);
+        System.out.println(pageSize);
+        List<Comment> comments = null;
+        try {
+            Query query = new Query();
+            query.addCriteria(Criteria.where("commodity_id").is(commodity_id).and("reply_to_comment_id").is("-1"));
+            query.with(Sort.by(Sort.Order.desc("createTime")));
+            query.skip((pageNum - 1) * pageSize).limit(pageSize);
+            comments = mongoTemplate.find(query, Comment.class);
+
+            for (Comment comment :
+                    comments) {
+                //查询这个的所有回复的评论
+                List<Comment> sub_comments = getSubComments(comment.getId());
+                comment.setSubComments((ArrayList<Comment>) sub_comments);
+            }
+            
+        } catch (Exception e) {
+            System.out.println("查询商品评论出错");
+            System.out.println(e);
+            return null;
+        }
+        return comments;
+    }
+
+    public List<Comment> getSubComments(String comment_id){
+        Query sub_query = new Query();
+        sub_query.addCriteria(Criteria.where("reply_to_comment_id").is(comment_id));
+        sub_query.with(Sort.by(Sort.Order.desc("createTime")));
+        List<Comment> sub_comments = mongoTemplate.find(sub_query, Comment.class);
+        if (sub_comments.size()>0){
+            for (Comment comment :
+                    sub_comments) {
+                comment.setSubComments((ArrayList<Comment>) getSubComments(comment.getId()));
+            }
+        }
+        return sub_comments;
+    }
+
+    public int addComment(Comment comment) {
+        try{
+            mongoTemplate.save(comment);
+        }catch (Exception e){
+            System.out.println(e);
+            return 0;
+        }
+        return 1;
+    }
+
+    public double getAvgCommodityRating(String commodity_id) {
+
+        List<CommentRepository.CommentAggregationResult> results = commentRepository.calculateAverageRatingByCommodityId(commodity_id);
+        if (!results.isEmpty()) {
+            CommentRepository.CommentAggregationResult result = results.get(0);
+            return result.getAverageRating();
+        }
+        return 0.0;
+    }
+}
